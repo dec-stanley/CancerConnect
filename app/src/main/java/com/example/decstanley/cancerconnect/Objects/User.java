e30d25ac84d2246d2755359042facc4ad9a81325
EmmanuelMadu <emmanuelmadu@hotmail.co.uk>
2018-11-04 23:39:29 +0000

    Added Classes

--- app/src/main/java/com/example/decstanley/cancerconnect/Objects/User.java
+++ app/src/main/java/com/example/decstanley/cancerconnect/Objects/User.java
@@ -1,4 +1,28 @@
 package com.example.decstanley.cancerconnect.Objects;
 
+import android.media.Image;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 public class User {
+
+    private char userID;
+    private String fullName;
+    private String username;
+    private String emailAddress;
+    private Date dob;
+    private Image profilePicture;
+    private boolean active;
+    private Date joinDate;
+    private ArrayList<Friend> friends;
+    private ArrayList<Post> posts;
+
+    public boolean sendMessage(){
+        return false;
+    }
+
+    public boolean reportUser(){
+        return false;
+    }
 }
