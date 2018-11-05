a0c4417ec6d5020569c25601701bd70fb0744914
Scott Heptinstall <scottyheppie@outlook.com>
2018-11-05 02:04:37 +0000

    UI assets

--- app/src/main/java/com/example/decstanley/cancerconnect/UI/MainActivity.java
+++ app/src/main/java/com/example/decstanley/cancerconnect/UI/MainActivity.java
@@ -1,13 +1,18 @@
-package com.example.decstanley.cancerconnect;
+package com.example.decstanley.cancerconnect.UI;
+
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.decstanley.cancerconnect.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.entry_screen);
+
+
     }
 }
---
+++ app/src/main/java/com/example/decstanley/cancerconnect/UI/RegisterActivity2.java
@@ -0,0 +1,4 @@
+package com.example.decstanley.cancerconnect.UI;
+
+public class RegisterActivity2 {
+}
