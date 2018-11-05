b589477ba9f553bf3d3c157ad6a99cae00accd75
Scott Heptinstall <scottyheppie@outlook.com>
2018-11-05 00:45:48 +0000

    Message, messages

--- app/src/main/java/com/example/decstanley/cancerconnect/Objects/Messages.java
+++ app/src/main/java/com/example/decstanley/cancerconnect/Objects/Messages.java
@@ -1,4 +1,21 @@
 package com.example.decstanley.cancerconnect.Objects;
 
+import java.util.ArrayList;
+
 public class Messages {
+    private ArrayList<Message> messages;
+    private String recipientID;
+
+    public Messages(String recipientID){
+        this.recipientID = recipientID;
+        messages = new ArrayList();
+    }
+
+    public String getRecipientID(){
+        return recipientID;
+    }
+
+    public void addMessage(Message message){
+        messages.add(message);
+    }
 }
