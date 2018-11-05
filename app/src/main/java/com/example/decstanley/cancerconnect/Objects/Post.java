bcdb8953fef8467cbd2eccfed294a24ea5380d8c
TOSHIBA-PC\My Laptop <b7014939@my.shu.ac.uk>
2018-11-05 00:45:10 +0000

    added comment, comments and started to work on post, posts, comment and comments. also added comparators for commentid and postid. need to finish function for removing object from post and comment collection classes.

--- app/src/main/java/com/example/decstanley/cancerconnect/Objects/Post.java
+++ app/src/main/java/com/example/decstanley/cancerconnect/Objects/Post.java
@@ -1,4 +1,33 @@
 package com.example.decstanley.cancerconnect.Objects;
 
+import java.util.ArrayList;
+
 public class Post {
+    private String postID;
+    private String userID;
+    private String information;
+    private ArrayList<String> likes;
+    private Comments comments;
+
+    public String getPostID() {
+        return this.postID;
+    }
+
+    public String getUserID() {
+        return this.userID;
+    }
+
+    public String getInformation() {
+        return this.information;
+    }
+
+    public void addLike(String userID) {
+        likes.add(userID);
+    }
+
+    public void addComment(String commentID, String userID, String comment) {
+        comments.addComment(new Comment(commentID, userID, comment));
+    }
+
+    public void openMoreOptions(){}
 }
--- app/src/main/java/com/example/decstanley/cancerconnect/Objects/Posts.java
+++ app/src/main/java/com/example/decstanley/cancerconnect/Objects/Posts.java
@@ -1,4 +1,17 @@
 package com.example.decstanley.cancerconnect.Objects;
 
+import java.util.ArrayList;
+
 public class Posts {
+    private ArrayList<Post> posts;
+
+    public void addPost(Post newPost)
+    {
+        posts.add(newPost);
+    }
+
+    public void deletePost(int postID)
+    {
+        //posts.remove();
+    }
 }
