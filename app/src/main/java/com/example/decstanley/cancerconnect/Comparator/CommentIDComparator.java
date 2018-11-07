33685fb91ebce7f09d981a6b600265e89add9d5e
TOSHIBA-PC\My Laptop <b7014939@my.shu.ac.uk>
2018-11-07 21:03:49 +0000

    removed comparator code

--- app/src/main/java/com/example/decstanley/cancerconnect/Comparator/CommentIDComparator.java
+++ app/src/main/java/com/example/decstanley/cancerconnect/Comparator/CommentIDComparator.java
@@ -1,9 +1,4 @@
 package com.example.decstanley.cancerconnect.Comparator;
 
-import com.example.decstanley.cancerconnect.Objects.Comment;
-
-import java.util.Comparator;
-
-class CommentIDComparator implements Comparator<Comment> {
-    public boolean equals(Comment a)
+class CommentIDComparator{
 }
