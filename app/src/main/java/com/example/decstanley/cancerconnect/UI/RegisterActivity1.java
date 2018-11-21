30e26646a9faef77f2b0fd24ca55e6db2a1b9eb2
EmmanuelMadu <emmanuelmadu@hotmail.co.uk>
2018-11-21 00:13:11 +0000

    Merge branch 'master' of https://github.com/dec-stanley/CancerConnect
    # Conflicts:
    #	app/src/main/java/com/example/decstanley/cancerconnect/UI/EntryActivity.java
    #	app/src/main/java/com/example/decstanley/cancerconnect/UI/LoginActivity.java
    #	app/src/main/java/com/example/decstanley/cancerconnect/UI/MainActivity.java
    #	app/src/main/res/layout/entry_screen.xml

--- app/src/main/java/com/example/decstanley/cancerconnect/UI/RegisterActivity1.java
+++ app/src/main/java/com/example/decstanley/cancerconnect/UI/RegisterActivity1.java
@@ -3,23 +3,37 @@ package com.example.decstanley.cancerconnect.UI;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.decstanley.cancerconnect.R;
 
 public class RegisterActivity1  extends AppCompatActivity {
+
+    private EditText nameEntry;
+    private EditText emailEntry;
+    private EditText passwordEntry1;
+    private EditText passwordEntry2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_screen1);
         setUpButtonListeners(); // sets up the button listeners
+
+        nameEntry = (EditText) findViewById(R.id.NameEntry);
+        emailEntry = (EditText) findViewById(R.id.EmailEntry);
+        passwordEntry1 = (EditText) findViewById(R.id.PasswordEntry);
+        passwordEntry2 = (EditText) findViewById(R.id.RepasswordEntry);
+
     }
 
     private void setUpButtonListeners(){
         /*< Backbutton Listener > */
-        // Listener for login button takes user from the main page to the login activity
         ImageButton backButton =  findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -28,11 +42,49 @@ public class RegisterActivity1  extends AppCompatActivity {
             }
         });
         /*< /Backbutton Listener > */
+
+        /*< ContinueButton Listener > */
+        Button contButton = (Button)  findViewById(R.id.ContinueButton);
+        contButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                if(isValidData()){
+                    moveToSecondRegisterPage();
+                }else{
+                    //TODO show error messages
+                }
+            }
+        });
+        /*< /ContinueButton Listener > */
+
     }
 
-    //takes the user back to the main activity
+    //Returns if the data entered is valid
+    private boolean isValidData(){
+        //TODO - DEC -
+        //Validation. get data and check if its valid bblah blah blah return true if all is valid return false if one or more are not valid
+        // an example to check if its empty
+        if(TextUtils.isEmpty(nameEntry.getText().toString().trim())){
+            Toast.makeText(this, "Please enter a name", Toast.LENGTH_LONG * 10).show();
+        }
+
+        return true;
+    }
+
+    //Takes the user back to the main activity
     private void backToMainActivity(){
         Intent i = new Intent(RegisterActivity1.this , MainActivity.class);
+        startActivity(i);
+    }
+
+    //Takes the user to the second register page and also passes the data from this page
+    private void moveToSecondRegisterPage(){
+        Intent i = new Intent(RegisterActivity1.this , RegisterActivity2.class);
+
+        i.putExtra("FULLNAME",nameEntry.getText().toString().trim());
+        i.putExtra("EMAIL", emailEntry.getText().toString().trim());
+        i.putExtra("PASSWORD", passwordEntry1.getText().toString().trim());
+
         startActivity(i);
     }
 }
--- app/src/main/java/com/example/decstanley/cancerconnect/UI/RegisterActivity2.java
+++ app/src/main/java/com/example/decstanley/cancerconnect/UI/RegisterActivity2.java
@@ -1,4 +1,108 @@
 package com.example.decstanley.cancerconnect.UI;
 
-public class RegisterActivity2 {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import com.example.decstanley.cancerconnect.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class RegisterActivity2 extends AppCompatActivity {
+    private EditText phoneNumber;
+    private EditText dob;
+    private String fullname, password, email;
+
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_screen2);
+        setUpButtonListeners();
+
+        // firebase auth stuff
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+        phoneNumber = (EditText) findViewById(R.id.PhoneNumberEntry);
+        dob = (EditText) findViewById(R.id.DOBEntry);
+
+        // get data from other page and set them to public variables
+        fullname = getIntent().getStringExtra("FULLNAME");
+        password = getIntent().getStringExtra("EMAIL");
+        email = getIntent().getStringExtra("PASSWORD");
+        
+    }
+
+    // sets up all listeners for the buttons on the activity
+    private void setUpButtonListeners() {
+        /*< Back button Listener > */
+        ImageButton backButton = findViewById(R.id.BackButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                backToRegisterActivity1();
+            }
+        });
+        /*< /Backbutton Listener > */
+
+        Button contButton = (Button)  findViewById(R.id.Sumbit);
+        contButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                if(isValidData()){
+                    if(enterData()){
+                        // move user to the feed activity
+                    }else{
+                        //TODO show error messages - DATA WAS NOT ENTERED INTO THE DATABASE
+                    }
+
+                }else{
+                    //TODO show error messages - DATA IS NoT VALID
+                }
+            }
+        });
+    }
+
+    // returns if data is entered
+    private boolean enterData(){
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(RegisterActivity2.this, "Entered", Toast.LENGTH_LONG * 10).show();
+                }else{
+                    Toast.makeText(RegisterActivity2.this, "NOT", Toast.LENGTH_LONG * 10).show();
+                }
+            }
+        });
+        return true;
+    }
+
+    //Returns if the data entered is valid
+    private boolean isValidData(){
+        //TODO - DEC -
+        //Validation. get data and check if its valid bblah blah blah return true if all is valid return false if one or more are not valid
+        return true;
+    }
+
+
+    //Takes the user back to the RegisterActivity2 activity
+    private void backToRegisterActivity1(){
+        Intent i = new Intent(RegisterActivity2.this , RegisterActivity1.class);
+        startActivity(i);
+    }
+
+
+
 }
